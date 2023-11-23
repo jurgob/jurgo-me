d6015addfc038ac5a855887a4b8b73d06fbcd382
jurgob <jurgob@users.noreply.github.com>
2023-11-23 15:55:45 +0000

    Updates

---
+++ component---src-pages-404-js-6bef1269e8a2d1e413a4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjurgo_me=self.webpackChunkjurgo_me||[]).push([[883],{9616:function(e,t,n){n.r(t);var u=n(7294),l=n(5127),r=n(3751);t.default=function(){return u.createElement(l.Z,null,u.createElement(r.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6bef1269e8a2d1e413a4.js.map
