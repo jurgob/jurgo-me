0a875ea49cfa535283aa1a484625b543506eec01
jurgob <jurgob@users.noreply.github.com>
2024-03-04 12:20:44 +0000

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,15 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-RV2YH4CV2S', // Google Analytics / GA
+        ],
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
